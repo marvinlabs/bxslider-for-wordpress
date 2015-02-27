980bb0ec944e71e7813ff566844b86a077f0db2b
Vincent Mimoun-Prat <vpratfr@gmail.com>
2015-02-27 15:55:07 +0100

    -

--- grunt/tasks/gitcommit.js
+++ grunt/tasks/gitcommit.js
@@ -1,11 +1,24 @@
 module.exports = function (grunt, options) {
+    var assetFiles = [];
+
+    var configs = [options.assets.styles, options.assets.scripts];
+    for (var j = 0; j < configs.count; ++j) {
+        var config = configs[j];
+        for (var i = 0; i < config.length; ++i) {
+            var filter = config[i].output.path + "/*.css";
+            assetFiles.push(filter);
+        }
+    }
+
     return {
         post_release: {
             options: {
-                comment: "Release v" + options.pkg.version
+                message: "Preparing release v" + options.pkg.version
             },
             files: {
-                versions: options.release.versionFiles
+                versions: options.release.versionFiles,
+                assets: assetFiles,
+                lang: [options.i18n.poPath + "/**"]
             }
         }
     };
