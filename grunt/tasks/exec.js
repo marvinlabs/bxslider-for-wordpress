4432a3061d9c4baed2be9b10fe493020d6c35d11
fxbenard <fxbenard@gmail.com>
2015-03-05 14:47:42 +0100

    Update pourcentage of TX pull to 95%

--- grunt/tasks/exec.js
+++ grunt/tasks/exec.js
@@ -1,7 +1,7 @@
 module.exports = function (grunt, options) {
     return {
         txpull: { // Pull Transifex translation - grunt exec:txpull
-            cmd: 'tx pull -a --minimum-perc=100' // Change the percentage with --minimum-perc=yourvalue
+            cmd: 'tx pull -a --minimum-perc=95' // Change the percentage with --minimum-perc=yourvalue
         },
         txpush_s: { // Push pot to Transifex - grunt exec:txpush_s
             cmd: 'tx push -s'
